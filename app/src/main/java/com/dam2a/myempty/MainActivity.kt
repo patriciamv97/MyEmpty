73cd262d16260c42cd67d9e5208ff384eb0ab97a
ivansteuerberg <irodriguezsteuerberg@danielcastelao.org>
2021-09-27 09:50:49 +0200

    Initial commit

---
+++ app/src/main/java/com/dam2a/myempty/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dam2a.myempty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
